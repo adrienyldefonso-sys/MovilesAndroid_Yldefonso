e3d45cfd7a6e43d73d771938d99057191e0bf96d
Becker Yldefonso <adrien.yldefonso@tecsup.edu.pe>
2026-09-22 17:34:44 -0500

    Agregacion de los modelos medico y cita para poder establecer los atributos,de igual manera el repositorio para poder establecer una fuente de datos para simular una bd

--- app/src/main/java/com/yldefonso/clinicasaludplus/model/Medico.kt
+++ app/src/main/java/com/yldefonso/clinicasaludplus/model/Medico.kt
@@ -1,2 +1,12 @@
 package com.yldefonso.clinicasaludplus.model
 
+//Representa al medico disponible y se establecen sus atributos
+data class Medico(
+    val id : Int,
+    val nombre : String,
+    val especialidad: String,
+    val calificacion : Double,
+    val resenas : Int,
+    val experienciaAnios: Int,
+    val descripcion : String
+)
