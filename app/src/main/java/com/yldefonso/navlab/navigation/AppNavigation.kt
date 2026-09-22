7421c95d9696e0760e1de6e6a113649dc6de50a9
Becker Yldefonso <adrien.yldefonso@tecsup.edu.pe>
2026-09-22 10:54:32 -0500

    Definicion del Screen con el selead class y los 4 objetos con sus parametros (Home,List,Profile,Detail) junto al AppNavigation con su controlador de navegacion,la pantalla con la que se comenzara y el registro de todas.

--- app/src/main/java/com/yldefonso/navlab/navigation/AppNavigation.kt
+++ app/src/main/java/com/yldefonso/navlab/navigation/AppNavigation.kt
@@ -1,2 +1,48 @@
 package com.yldefonso.navlab.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+
+@Composable
+fun AppNavigation(){
+    //Crea y recuerda el controlador de navegacion LUEGO DE LAS RECOMPOSICIONES
+    val navController = rememberNavController()
+
+    NavHost(
+        navController= navController,
+        //Se designa la pantalla donde se comienza
+        startDestination= Screen.Home.route
+    ){
+        //registra HomeScreen como la ruta "home"
+        composable(Screen.Home.route){
+            HomeScreen(navController)
+        }
+        //registra ListScreen bajo la ruta "list"
+        composable(Screen.List.route){
+            ListScreen(navController)
+        }
+        //registra ProfileScreen como la ruta "profile"
+        composable(Screen.Profile.route){
+            ProfileScreen(navController)
+        }
+        //registrDetailScreen pero este recibe un elemento itemId
+        composable(
+            route=Screen.Detail.route,
+            arguments=listOf(
+                navArgument(name ="itemId") {
+                    type = NavType.IntType //-> El argumento itemId es INT
+                    defaultValue=0
+                }
+            )
+        ){backStackEntry->
+            //Extrae el itemId,si no existe,usa 0
+            val itemId = backStackEntry.arguments?.getInt(key="itemId")?:0
+            DetailScreen(navController,itemId)
+        }
+    }
+}
--- app/src/main/java/com/yldefonso/navlab/navigation/Screen.kt
+++ app/src/main/java/com/yldefonso/navlab/navigation/Screen.kt
@@ -1,4 +1,25 @@
+//Este archivo se encarga de todas las rutas a traves de sealed class.Recibe route como
+//parametro el cual es el IDENTIFICADOR UNICO de cada pantalla.Al ser sealed,el compilador conoce
+//todas las rutas posibles en compilacion.
 package com.yldefonso.navlab.navigation
+import com.yldefonso.navlab.navigation.Screen
+sealed class Screen (val route:String) {
 
-class Screen {
+    //PANTALLA DE INICIO
+    object Home : Screen("home")
+
+    //LISTA DE ELEMENTOS
+    object List : Screen("list")
+
+    //PERFIL USUARIO
+    object Profile : Screen("profile")
+
+    //ruta con argumento: itemid es el placeholder(elemento temporal)  el cual Navigation reemplaza
+    //con el valor real cuando se navega
+
+    object Detail : Screen("detail/{itemId}") {
+        fun createRoute(itemId: Int): String = "detail/$itemId"
+    }
 }
+      //Construye la ruta final y el placeholder reemplaza la ruta con el valor real(id del item)
+      //Ejemplo:detail/5-> viene siendo ese String el que pasa a navController
