505f85bdfd74ad0f21811c2ae51e132af0c50a18
Becker Yldefonso <adrien.yldefonso@tecsup.edu.pe>
2026-09-22 11:31:32 -0500

    Codificacion del archivo ListScreen y HomeScreen configurando el dieseñp que tendran (a traves de Column,TopAppBar,LazyColumn) junto a la navegacion con lo que nos mostrara en la pantalla

--- app/src/main/java/com/yldefonso/navlab/navigation/AppNavigation.kt
+++ app/src/main/java/com/yldefonso/navlab/navigation/AppNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.yldefonso.navlab.screens.HomeScreen
 
 @Composable
 fun AppNavigation(){
--- app/src/main/java/com/yldefonso/navlab/screens/HomeScreen.kt
+++ app/src/main/java/com/yldefonso/navlab/screens/HomeScreen.kt
@@ -1,2 +1,52 @@
 package com.yldefonso.navlab.screens
 
+import android.R.attr.onClick
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.yldefonso.navlab.navigation.Screen
+
+@Composable
+fun HomeScreen(navController: NavController){
+    Column(
+        modifier= Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = ("Pantall Tecsup"),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        //Boton relleno que permitira navegar la lista
+        Button(
+            onClick = { navController.navigate(Screen.List.route) },//Se tiene que esocoger el navigate de NAVDORECTIONS
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Ver lista con elementos")
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        //Aca se añade el boton con borde que navega AL PERFIL
+        OutlinedButton(
+            onClick = { navController.navigate(Screen.Profile.route) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Mi perfil")
+        }
+    }
+    }
